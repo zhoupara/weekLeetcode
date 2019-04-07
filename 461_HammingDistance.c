77817775e272c6ab7ba9574d3f160f96aca31a76
para <zhou_para@163.com>
2019-04-07 17:23:59 +0800

    4月7日

--- 461_HammingDistance.c
+++ 461_HammingDistance.c
@@ -1,6 +1,8 @@
 /* ************************************************************************
  *       Filename:  461_HammingDistance.c
- *    Description:  
+ *    Description:  整数两个之间的汉：明距离
+ *					指的的英文这两个数字对应二进制位不同的位置的数目。
+ *					两个给出整数x状语从句：y，计算它们之间的汉明距离。
  *        Version:  1.0
  *        Created:  2019年04月03日 20时44分30秒
  *       Revision:  none
@@ -10,7 +12,15 @@
  * ************************************************************************/
 
 int HammingDistance(int x, int y) {
-
+	if (x <= 0 && y <= 0 && x > 0x7fffffff && y > 0x7fffffff) 
+       return -1;
+    int tmp = x ^ y;
+    int n = 0;
+    while (tmp) {
+        tmp &= (tmp -1);
+        n++;
+    }
+    return n;
 }
 
 
